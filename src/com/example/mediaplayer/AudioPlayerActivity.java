d6f618052a4f932096237c955563b268a8427636
chris <chris@10.52.242.159>
2014-11-11 19:15:32 +0000

    slider and timer

--- src/com/example/mediaplayer/AudioPlayerActivity.java
+++ src/com/example/mediaplayer/AudioPlayerActivity.java
@@ -7,40 +7,53 @@ import android.content.Context;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.Chronometer;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class AudioPlayerActivity extends ActionBarActivity {
-	
+
 	private State currentState;
 	private MediaPlayer mp;
 	private Button audioPlayButton;
 	private Button audioPauseButton;
 	private Button audioStopButton;
 	private String audioPath;
+	private SeekBar seekbar;
+	private Handler seekHandler = new Handler(); /** Called when the activity is first created. */ 
 	private int time = 0;
+	private TextView textview;
+	private TextView timer;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_audio_player);
-		
+
 		Intent myIntent = getIntent();
 		audioPath = myIntent.getStringExtra("path");
-		
+
 		audioPlayButton = (Button)findViewById(R.id.playButtonAudio);
 		audioPauseButton = (Button)findViewById(R.id.pauseButtonAudio);
 		audioStopButton = (Button)findViewById(R.id.stopButtonAudio);
-		
+		textview = (TextView)findViewById(R.id.textView1);
+		seekbar = (SeekBar)findViewById(R.id.musicSeekBar);
+		seekHandler = new Handler();
+		timer = (TextView)findViewById(R.id.seekBarUpdate);
+
 		currentState = new AudioPlayState(this);
 		mp = new MediaPlayer();
 		try {
 			mp.setDataSource(audioPath);
 			mp.prepare();
+			seekbar.setMax(mp.getDuration());
 		} catch (IllegalArgumentException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
@@ -54,39 +67,41 @@ public class AudioPlayerActivity extends ActionBarActivity {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
+
 		restoreFromMemento();
-		
+
 		mp.seekTo(time);
 		mp.start();
-		
+		textview.setText(audioPath.substring(17));
+		seekUpdation();
+
 		audioPlayButton.setOnClickListener(new OnClickListener(){
 
 			@Override
 			public void onClick(View v) {
 				AudioPlayerActivity.this.currentState.play();
 			}
-			
+
 		});
-		
+
 		audioPauseButton.setOnClickListener(new OnClickListener(){
 
 			@Override
 			public void onClick(View v) {
 				AudioPlayerActivity.this.currentState.pause();
 			}
-			
+
 		});
-		
+
 		audioStopButton.setOnClickListener(new OnClickListener(){
 
 			@Override
 			public void onClick(View v) {
 				AudioPlayerActivity.this.currentState.stop();
 			}
-			
+
 		});
-		
+
 		try {
 			mp.setDataSource(audioPath);
 		} catch (IllegalArgumentException e) {
@@ -102,37 +117,51 @@ public class AudioPlayerActivity extends ActionBarActivity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		Context context = getApplicationContext();
-		Toast toast = Toast.makeText(context, audioPath, Toast.LENGTH_SHORT);
-		toast.show();
 	}
-	
+
+	Runnable run = new Runnable() { 
+		@Override 
+		public void run() { 
+			seekUpdation(); 
+		} 
+	}; 
+
+	public void seekUpdation() { 
+		int i = mp.getCurrentPosition();
+		seekbar.setProgress(i);
+		if(i%60000/1000 < 10) timer.setText(i/60000 + ":0" + (i%60000)/1000);
+		else timer.setText(i/60000 + ":" + (i%60000)/1000);
+		seekHandler.postDelayed(run, 1000); 
+	} 
+
+    @Override
 	public void onBackPressed() {
-		this.currentState.pause();
-		this.finish();
+		// TODO Auto-generated method stub
+		super.onBackPressed();
 	}
-	
-	public void onStop() {
-		this.currentState.pause();
-		this.onDestroy();
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
 	}
-	
-    public void setTime(int time) {
-        this.time = time;
-    }
- 
-    public Memento saveToMemento() {
-        return new Memento(Integer.toString(this.time), this.audioPath);
-    }
- 
-    public void restoreFromMemento() {
-    	for (int u = 0; u < MainActivity.getCaretaker().myMementos.size(); u++) {
-    		Memento memento = MainActivity.getCaretaker().myMementos.get(u);
-    		if (memento.getPath().equals(audioPath)) {
-    			setTime(Integer.parseInt(memento.getState()));
-    		}
-    	}
-    }
+
+	public void setTime(int time) {
+		this.time = time;
+	}
+
+	public Memento saveToMemento() {
+		return new Memento(Integer.toString(this.time), this.audioPath);
+	}
+
+	public void restoreFromMemento() {
+		for (int u = 0; u < MainActivity.getCaretaker().myMementos.size(); u++) {
+			Memento memento = MainActivity.getCaretaker().myMementos.get(u);
+			if (memento.getPath().equals(audioPath)) {
+				setTime(Integer.parseInt(memento.getState()));
+			}
+		}
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -152,15 +181,15 @@ public class AudioPlayerActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
+
 	public void setState(State s) {
 		this.currentState = s;
 	}
-	
+
 	public class AudioPlayState implements State {
-		
+
 		AudioPlayerActivity myAudioPlayerActivity;
-		
+
 		public AudioPlayState(AudioPlayerActivity v) {
 			this.myAudioPlayerActivity = v;
 		}
@@ -188,11 +217,11 @@ public class AudioPlayerActivity extends ActionBarActivity {
 			myAudioPlayerActivity.mp.pause();
 		}
 	}
-	
+
 	public class AudioPauseState implements State {
-		
+
 		AudioPlayerActivity myAudioPlayerActivity;
-		
+
 		public AudioPauseState(AudioPlayerActivity v) {
 			this.myAudioPlayerActivity = v;
 		}
@@ -217,11 +246,11 @@ public class AudioPlayerActivity extends ActionBarActivity {
 			myAudioPlayerActivity.mp.pause();
 		}
 	}
-	
+
 	public class AudioStopState implements State {
-		
+
 		AudioPlayerActivity myAudioPlayerActivity;
-		
+
 		public AudioStopState(AudioPlayerActivity v) {
 			this.myAudioPlayerActivity = v;
 		}
@@ -247,9 +276,5 @@ public class AudioPlayerActivity extends ActionBarActivity {
 			Toast toast = Toast.makeText(context, "audio is already stopped", Toast.LENGTH_SHORT);
 			toast.show();
 		}		
-	}
-	
-	public void exit(View v){
-		this.onDestroy();
 	}
 }
