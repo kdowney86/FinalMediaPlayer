be01429b7fa01f846c6532993756bf15514ad927
chris <chris@10.52.242.58>
2014-11-13 09:00:31 +0000

    [Chris] playlist items sorted

--- src/com/example/mediaplayer/PlaylistFactory.java
+++ src/com/example/mediaplayer/PlaylistFactory.java
@@ -2,13 +2,29 @@ package com.example.mediaplayer;
 
 public class PlaylistFactory {
 
+	private AudioPlaylist audioPlaylist;
+	private VideoPlaylist videoPlaylist;
+	private Media audioMedia = new Media();
+	private Media videoMedia = new Media();
+	
 	public Playlist getPlaylist(Media m){
-			int i = 0;
+		
+		audioPlaylist = new AudioPlaylist(audioMedia);
+		videoPlaylist = new VideoPlaylist(videoMedia);
+		
+		for(int i = 0; i < m.mediaGroup().size(); i++){
 			if(m.get(i).type().equals("mp3")){
-				return new AudioPlaylist(m);
+				audioMedia.add(m.get(i));
 			}else if(m.type().equals("mp4")){		
-				return new VideoPlaylist(m);
+				videoMedia.add(m.get(i));
 			}
+		}
+		if(audioMedia.mediaGroup().size() > 0){
+			audioPlaylist = new AudioPlaylist(audioMedia);
+		}
+		if(videoMedia.mediaGroup().size() > 0){
+			videoPlaylist = new VideoPlaylist(videoMedia);
+		}
 		return null;		
 	}
 }
